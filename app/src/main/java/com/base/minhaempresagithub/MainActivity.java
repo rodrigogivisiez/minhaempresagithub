c8d158168857632c15c0408430af1c86d46d3eab
rodrigogivisiez <rodrigo2017@gmail.com>
2018-07-01 19:27:02 -0300

    other commit

--- app/src/main/java/com/base/minhaempresagithub/MainActivity.java
+++ app/src/main/java/com/base/minhaempresagithub/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "adfasf", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "adfasf", Toast.LENGTH_SHORT).show();
+
     }
 }
